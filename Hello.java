4615187316cfb2d090ec2339595720fb58e4002e
MalcolmXSimien <46726569+MalcolmXSimien@users.noreply.github.com>
2019-01-15 15:18:27 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/*************************************************************************** 
+ * Java Program Name 
+ *************************************************************************** 
+ * Lab 1  
+ *__________________________________________________________________________ 
+ * Malcolm Simien  
+ * January 15, 2019  
+ *225 002 
+ ****************************************************************************/
+ 
+ 
+public class Hello { 
+ public static void printHeading() { 
+  System.out.println("Malcolm"); 
+  System.out.println("Lab 1"); 
+  System.out.println("Hello Java"); 
+  System.out.println("January 15, 2019"); 
+  System.out.println("225 002"); 
+  System.out.println(); 
+ } 
+ 
+ public static void main(String [] args) { 
+  printHeading(); 
+  System.out.println("Hello"); 
+ }  
+} 
